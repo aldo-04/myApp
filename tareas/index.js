e0fa0f19a5dc5ed039b003f4c0d2e218bc1620ea
aldo-04 <ordenaldo@gmail.com>
2021-06-07 17:59:29 -0300

    agrega funciones al modulo tareas

--- tareas/index.js
+++ tareas/index.js
@@ -1,5 +1,13 @@
 const fs=require("fs")
 
 module.exports={
-    leerJson: ()=> fs.readFileSync("./data/tareas.json",)
+    leerJson: ()=> fs.readFileSync("./data/tareas.json","utf-8"),
+    guardarTarea: (titulo,estado)=>{
+        let tareas = this.leerJson
+        let tarea={
+            titulo,
+            estado
+        }
+    tareas.push(tarea)
+    }
 }
